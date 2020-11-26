b89930949056946d35cd83fd23a667d98c38c714
richcheng <richcheng@msn.com>
2020-11-25 21:12:19 -0500

    Add project files.

---
+++ ConsoleParametric/InterviewProblem.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+namespace InterviewProblem
+{
+    public class System
+    {
+        /// <summary>
+        /// Constructor for initialzing the system given an array of integers
+        /// </summary>
+        /// <param name="input"></param>
+        List<int> list = new List<int>();
+        public System(int[] input)
+        {
+            // for (i = 0; <= input.length, i++)
+            // {
+            //     this.Add(input[i]);
+            // }
+
+            this.list = new List<int>(input);
+            Console.WriteLine("\nInitial input/list:");
+            list.ForEach(i => Console.Write("{0}\t", i));
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Add a new number to the system
+        /// </summary>
+        /// <param name="number"></param>
+        public void Add(int number)
+        {
+            this.list.Add(number);
+            Console.WriteLine("\nAdd: " + number.ToString() + "\nNowInput List =");
+            list.ForEach(i => Console.Write("{0}\t", i));
+        }
+
+        /// <summary>
+        /// Calculate the average of the numbers currently in the system
+        /// </summary>
+        /// <returns>The average as defined by number total divided by the total count of numbers</returns>
+        public double GetAverage()
+        {
+            double avg = this.GetSum(0, this.list.Count) / this.list.Count;
+            Console.WriteLine("\nGetAverage:" + avg.ToString());
+            return avg;
+        }
+
+        /// <summary>
+        /// Count the number of times appears in the system
+        /// </summary>
+        /// <param name="number">The number to be counted</param>
+        /// <returns></returns>
+        public int GetCount(int number)
+        {
+            int count = 0;
+            foreach (int item in this.list)
+            {
+                if (number == item)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\nGetCount:" + count.ToString());
+            return count;
+
+        }
+
+        /// <summary>
+        ///    Given a range designated by the start and end positions, 
+        ///    Calculate the sum between them. This should inclusive
+        /// </summary>
+        /// <param name="start">Start position</param>
+        /// <param name="end">Ending position</param>
+        /// <returns></returns>
+        public int GetSum(int start, int end)
+        {
+            int sum = 0;
+            foreach (int item in this.list)
+            {
+                sum += item;
+            }
+            Console.WriteLine("\nGetSum:" + sum.ToString());
+            return sum;
+        }
+    }
+}
---
+++ ConsoleParametric/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InterviewProblem;
+namespace ConsoleParametric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main...");
+            var input = new int[] { 8, 3, 5, 4, 5 };
+            var solution = new InterviewProblem.System(input);
+            solution.Add(5);
+            solution.GetAverage(); //should be 30
+            solution.GetCount(5);  // should have 3 '5'
+            solution.GetSum(0, 1); // should be 11
+            Console.WriteLine("press any key to quit");
+            Console.ReadLine();
+        }
+    }
+}
